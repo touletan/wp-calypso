5e273f7322cd51df7f378b506c8e5266bc907f90
Markus Dobmann <markus@mpnjs.com>
2018-09-21 15:54:57 +0200

    danger: remove 'green' flag if no warnings appear

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,7 @@
 /**
  * External dependencies
+ *
+ * @format
  */
 
 import { danger, warn, markdown, results } from 'danger';
@@ -30,7 +32,7 @@ if ( ! ghLabels.find( l => l.name.toLowerCase().includes( '[status]' ) ) ) {
 
 // Test instructions
 if ( ! danger.github.pr.body.includes( 'Testing instructions' ) ) {
-	warn( '"Testing instructions" are missing for this PR. Please add some' );
+	warn( '"Testing instructions" are missing for this PR. Please add some.' );
 }
 
 // skip if there are no warnings.
@@ -38,6 +40,4 @@ if ( results.warnings.length > 0 || results.fails.length > 0 ) {
 	markdown(
 		"This is an automated check which relies on [`PULL_REQUEST_TEMPLATE`](https://github.com/Automattic/wp-calypso/blob/master/.github/PULL_REQUEST_TEMPLATE.md). We encourage you to follow that template as it helps Calypso maintainers do their job. If you think 'Testing instructions' are not needed for your PR - please explain why you think so. Thanks for cooperation :robot:"
 	);
-} else {
-	markdown( "That's a great PR description, thank you so much for your effort!" );
 }
