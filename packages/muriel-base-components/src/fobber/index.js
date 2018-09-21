9cfbbaebb7226af68af92c77cb022eac0d0457b5
Ben Lowery <ben.lowery@automattic.com>
2018-09-21 17:28:26 -0400

    add script to build packages

---
+++ packages/muriel-base-components/src/fobber/index.js
@@ -0,0 +1,20 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+
+/**
+ * Internal dependencies
+ */
+
+/**
+ * A Fobber for fobbing
+ *
+ * @export
+ * @param {*} props The props to use
+ * @returns {React.ReactElement} a rendered Fobber
+ */
+export default function Fobber( props ) {
+	return <div className="muriel fobber">{ props.children }</div>;
+}
